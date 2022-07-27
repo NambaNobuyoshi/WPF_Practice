4f2eb5b41f6857c38eb4d316dfa4d19fff96e6b9
Nobuyoshi Namba <ramennobu0421@gmail.com>
2022-07-27 20:56:48 +0900

    20220727 アプリ終了時のメソッドの作成

---
+++ C_WPF_APP/C_WPF_APP/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace C_WPF_APP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 「×」ボタン押下時の処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            var Result = MessageBox.Show("アプリケーションを終了しますか", "確認", MessageBoxButton.OKCancel, MessageBoxImage.Warning);
+            if (Result == MessageBoxResult.OK)
+            {
+                Environment.Exit(0);
+            }
+        }
+    }
+}
---
+++ C_WPF_APP/C_WPF_APP/ViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_WPF_APP
+{
+    /// <summary>
+    /// バインディング用クラス
+    /// </summary>
+    internal class ViewModel
+    {
+    }
+}
